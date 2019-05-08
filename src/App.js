31c5aa656429ff3816e282c493e080e4a1807134
Grant Woods <grantdwoods@outlook.com>
2019-05-08 13:56:40 -0700

    Altering css style object
    quick example/reminder that you can do whatever you want to the "style" object that was defined before it is returned with JSX

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -64,6 +64,8 @@ class App extends Component {
         }
       </div> 
       );
+
+      style.backgroundColor = 'red';
     }
 
     return (

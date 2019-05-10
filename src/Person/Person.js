88cc4c59af45acc097ed350e9d97eae48108689c
Grant Woods <grantdwoods@outlook.com>
2019-05-10 16:32:26 -0700

    Small change to naming

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import style from './Person.module.css';
+import classes from './Person.module.css';
 
 const person = (props) => {
     console.log('Returning Person element');
 
     return(
-        <div className={style.Person}>
+        <div className={classes.Person}>
             <p onClick={props.click}>My name is {props.name} and I am {props.age}</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />

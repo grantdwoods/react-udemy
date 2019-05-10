0d94ddb625d24a3e4f19faa6940e1ac98937a967
Grant Woods <grantdwoods@outlook.com>
2019-05-10 16:30:30 -0700

    CSS modules
    No longer need to edit the project configuration to enable CSS modules as of react-scripts 2.0.0 and higher.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.module.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -71,19 +71,19 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
-    const classes = [];
+    const assignedClasses = [];
 
     if(this.state.persons.length <=2 ){
-      classes.push('red');
+      assignedClasses.push( classes.red );
     }
     if(this.state.persons.length <=1 ){
-      classes.push('bold');
+      assignedClasses.push( classes.bold );
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Getting started with componenets</h1>
-        <p className={classes.join(' ')}>Testing classes variable</p>
+        <p className={assignedClasses.join(' ')}>Testing classes variable</p>
         <button 
           style={style}
           onClick={this.togglePersonHandler}>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Person.css';
+import style from './Person.module.css';
 
 const person = (props) => {
     console.log('Returning Person element');
 
     return(
-        <div className='Person'>
+        <div className={style.Person}>
             <p onClick={props.click}>My name is {props.name} and I am {props.age}</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />

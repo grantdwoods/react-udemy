2dea1801530b1de1bb9836d0975b320aad8d6855
Grant Woods <grantdwoods@outlook.com>
2019-05-15 13:26:28 -0700

    Functional component optimization
    with React.memo, passing in a value instead of a reference to prevent cockpit re-render

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -33,10 +33,10 @@ const Cockpit = props => {
     buttonClass = classes.Red;
   }
 
-  if(props.persons.length <=2 ){
+  if(props.personsLength <=2 ){
     assignedClasses.push( classes.red ); //classes=['red']
   }
-  if(props.persons.length <=1 ){
+  if(props.personsLength <=1 ){
     assignedClasses.push( classes.bold ); // classes=['red', 'bold']
   }
 
@@ -53,4 +53,4 @@ const Cockpit = props => {
   );
 }
 
-export default Cockpit;
+export default React.memo(Cockpit);
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -79,7 +79,7 @@ class App extends Component {
       <button onClick={()=>{this.setState({showCockPit:false})}}>remove cockpit</button>
         {this.state.showCockPit ?<Cockpit 
           showPersons={this.state.showPersons}
-          persons={this.state.persons}
+          personsLength={this.state.persons.length}
           clicked={this.togglePersonHandler}
         />: null}
         {persons}

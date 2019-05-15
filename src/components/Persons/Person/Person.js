9b6fdfe6d9ad86d0fd0c06729fbd164a9b090270
Grant Woods <grantdwoods@outlook.com>
2019-05-15 12:18:18 -0700

    Logs for update lifecycle

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,6 +3,28 @@ import classes from './Person.module.css';
 
 class Person extends Component{
 
+    static getDerivedStateFromProps(props, state){
+        console.log('Persons getDerivedStateFromProps');
+        return state;
+    }
+
+    shouldComponentUpdate(nextProps,nextState){
+        console.log('Persons shouldComponentUpdate');
+
+        //Must return a bool. Typically you would compare props first.
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('Person getSnapshotBeforeUpdate');
+        return {message: 'snapshot'};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+        console.log('Person componentDidUpdate');
+        console.log('snapshot', snapshot);
+    }
+
     render(){
         console.log('Person render');
     

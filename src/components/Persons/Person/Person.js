df29e23258d3a8d6e7d8770fccef9eabc590b224
Grant Woods <grantdwoods@outlook.com>
2019-05-16 14:20:16 -0700

    Passing unknown props
    With the spread operator

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import classes from './Person.module.css';
+import withClass from '../../HOC/withClass';
 
 class Person extends Component{
 
@@ -16,4 +17,4 @@ class Person extends Component{
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);

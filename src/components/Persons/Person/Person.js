1df12e654bd6f8224c71b9ed694b7e021540a23f
Grant Woods <grantdwoods@outlook.com>
2019-05-16 14:54:09 -0700

    Using React.createRef()
    Focusing the last input rendered using the ref prop. This property can be used on any element.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,6 +5,15 @@ import PropTypes from 'prop-types';
 
 class Person extends Component{
 
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount(){
+        this.inputElementRef.current.focus();
+    }
+
     render(){
         console.log('Person render');
     
@@ -12,7 +21,12 @@ class Person extends Component{
             <Fragment>
                 <p onClick={this.props.click}>My name is {this.props.name} and I am {this.props.age}</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input 
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}
+                    ref={this.inputElementRef}
+                />
             </Fragment>
         );
     }

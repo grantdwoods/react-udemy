a44c7e82775a71384261b1c429a01a2f633628e2
Grant Woods <grantdwoods@outlook.com>
2019-05-16 16:36:26 -0700

    More set up

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Layout = (props) => (
+    <Fragment>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Fragment>
+);
+
+export default Layout;

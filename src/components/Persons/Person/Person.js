dbe607849db0c8e7f2dd97fbd2b42dcab7a38ff3
Grant Woods <grantdwoods@outlook.com>
2019-05-16 15:46:18 -0700

    Static context type
    For classes, allowing you more access to the context (in a lifecycle method for example)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,8 +11,11 @@ class Person extends Component{
         this.inputElementRef = React.createRef();
     }
 
+    static contextType = AuthContext;
+
     componentDidMount(){
         this.inputElementRef.current.focus();
+        console.log(this.context.authenticated);
     }
 
     render(){
@@ -20,9 +23,7 @@ class Person extends Component{
     
         return(
             <Fragment>
-            <AuthContext.Consumer>
-                {(context)=> context.authenticated ? <p>Authenticated</p> : <p>Please log in</p>}
-            </AuthContext.Consumer>
+                {this.context.authenticated ? <p>Authenticated</p> : <p>Please log in</p>}
                 <p onClick={this.props.click}>My name is {this.props.name} and I am {this.props.age}</p>
                 <p>{this.props.children}</p>
                 <input 

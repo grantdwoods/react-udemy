5122134de9534eef2d0e1f7509f041f5a859f003
Grant Woods <grantdwoods@outlook.com>
2019-05-17 16:29:38 -0700

    Hard coded example burger

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = props => {
     return (
-        <div>
-        
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"></BurgerIngredient>
+            <BurgerIngredient type="cheese"></BurgerIngredient>
+            <BurgerIngredient type="meat"></BurgerIngredient>
+            <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
 };
--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './BurgerIngredient.module';
+import classes from './BurgerIngredient.module.css';
 import PropTypes from 'prop-types';
 
 const BurgerIngredient = props => {

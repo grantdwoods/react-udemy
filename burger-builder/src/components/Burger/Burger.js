b2be630b283af8536afe5e900f16c43d1ffc19a5
Grant Woods <grantdwoods@outlook.com>
2019-05-17 17:05:52 -0700

    Dynamic Burger ingredients
    Based on state managed in BurgerBuilder container component.

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,21 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = props => {
+    const burgerIngredients = Object
+        .keys(props.ingredients)
+            .map((ingredientKey)=>{
+                return ([...Array(props.ingredients[ingredientKey])]
+                    .map(( _ , i)=>{
+                        return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>
+                    })
+                )
+            }
+        );
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+            {burgerIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,16 +2,28 @@ import React, {Component, Fragment} from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+    // Recall alt syntax for property initilization
+    // constructor(props){
+    //     super(props);
+    //     this.state = {...};
+    // }
+    state = {
+        ingredients:{
+            lettuce:1,
+            bacon:1,
+            cheese:2,
+            meat:2
+        },
+    };
 
     render(){
         return(
             <Fragment>
-                <Burger></Burger>
+                <Burger ingredients={this.state.ingredients}></Burger>
                 <div>Controls</div>
             </Fragment>
         );
     }
-
 }
 
 export default BurgerBuilder;

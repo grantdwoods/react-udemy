8b10d12d3c96f65af91e15836c574ec935352a0d
Grant Woods <grantdwoods@outlook.com>
2019-05-17 17:47:36 -0700

    Controls in place

--- burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,9 +1,9 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import classes from './BuildControl.module.css';
 
-const BuildControl = () => (
+const BuildControl = (props) => (
     <div className={classes.BuildControl}>
-        <div className={classes.Label}>{tsPropertySignature.label}</div>
+        <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>Less</button>
         <button className={classes.More}>More</button>
     </div>
--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
 
-const BuildControls = props = (
-    <div></div>
+const controls = [
+    {label: 'Lettuce', type: 'lettuce'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Meat', type: 'meat'},
+    {label: 'Cheese', type: 'cheese'}
+];
+
+const BuildControls = props => (
+    <div className={classes.BuildControls}>
+        {
+            controls.map( control => (
+                <BuildControl key={control.label} label={control.label}></BuildControl>)
+            )
+        }
+    </div>
 );
 
 export default BuildControls;

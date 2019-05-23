ba88e6b57b0cd2d4ebbdb6e0125eb7f0127e8898
Grant Woods <grantdwoods@outlook.com>
2019-05-22 17:59:18 -0700

    SideDrawer responsive to screen size

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React, {Fragment} from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const Layout = (props) => (
     <Fragment >
         <Toolbar/>
+        <SideDrawer></SideDrawer>
         <main className={classes.Content}>
             {props.children}
         </main>
